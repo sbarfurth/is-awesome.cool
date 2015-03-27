4f7410f0ad852c2faea9bb77e1121d818ae860a6
aFreshMelon <github@afreshmelon.com>
2015-03-27 16:31:30 +0100

    Add instructions and better assets

---
+++ README.md
@@ -0,0 +1,13 @@
+# is-awesome.cool
+
+Installation is very simple.
+
+Simply clone the repository, switch into it and run a few commands.
+
+```
+bundle
+bundle exec sass views/app.sass views/app.css
+bundle exec ruby app.rb
+```
+
+That should do it. The app is available on ``http://localhost:4567``
--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
 require 'sinatra'
 require 'slim'
-require 'sass'
 require 'compass'
-
-configure do
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = 'views'
-  end
-
-  set :sass, Compass.sass_engine_options
-end
-
-get '/app.css' do
-  sass :app
-end
 
 get '/:name' do
   slim :index
