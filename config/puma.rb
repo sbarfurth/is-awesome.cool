30cc4696bd09dc450c191d9ece6c90e2e89c4984
Sebastian Barfurth <sebibarfurth@gmail.com>
2015-11-27 20:39:28 +0100

    Properly fix bind

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,8 @@
-shared_dir = "tmp/puma"
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/tmp/puma"
 pidfile "#{shared_dir}/pid"
 state_path "#{shared_dir}/state"
 environment "production"
 
-bind "unix:/#{shared_dir}/puma.sock"
+bind "unix://#{shared_dir}/puma.sock"
 activate_control_app
