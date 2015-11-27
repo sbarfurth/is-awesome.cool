ba773752fc527e774bc5a5faa7307d853c71762f
Sebastian Barfurth <sebibarfurth@gmail.com>
2015-11-27 20:25:55 +0100

    Update puma config for upstart

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,5 @@
-workers 1
-threads 1, 6
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "/tmp/is-awesome.cool"
-daemonize true
+shared_dir = "tmp/puma"
+pidfile "#{shared_dir}/pid"
+state_path "#{shared_dir}/state"
 environment "production"
-bind "unix://#{shared_dir}/sockets/puma.sock"
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
